06fcd63e3ad169f4edbd8969c5790ba62ad4f894
Louie Chen & Laura <pair+louiehchen.lrrnlds@devbootcamp.com>
2016-12-08 15:31:53 -0800

    finish sessions + users controllers, added routes for users and sessions

--- rottenimdb/config/routes.rb
+++ rottenimdb/config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   root 'movies#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  resources :genres
+
+
 end
