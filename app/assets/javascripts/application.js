64d16c828c1e54a768be64bc5e4db37561be3742
Louie Chen <louiehchen@gmail.com>
2016-12-09 11:58:44 -0800

    beta upvote functionality for reviews and comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,19 @@ function filterFunction () {
     }
   }
 }
+
+function upvoteButton () {
+  $(".upvote").on("click", ".upvote_button", function(event){
+    event.preventDefault();
+    var $this = $(this).parents();
+    var url = $this.attr("action");
+    var method = $this.attr("method");
+    var vote = $this.siblings("p").children(".vote");
+    $.ajax({
+      url: url,
+      method: method
+    }).done(function(response){
+      $(vote).text(response)
+    })
+    })
+}
