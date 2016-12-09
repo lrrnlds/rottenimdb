421c2f02bd98bd5d9d7c7532c5be687ef9320a44
Alex Nelson & Amrin Grewal <pair+Alex-CAN.amrinder86@devbootcamp.com>
2016-12-08 22:17:06 -0800

    reviews controller

---
+++ app/controllers/concerns/reviews_controller.rb
@@ -0,0 +1,41 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    review = Review.new(review_params)
+      review.vote_count = 0
+      review.user_id = 1
+    if review.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+  def update
+    review = Review.find(params[:id])
+    if review.update_attributes(user_params)
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:title, :body, :rating, :movie_id)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,11 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
   get '/signup' => 'users#new'
-  post '/users' => 'users#create'
 
-  get '/users/:id' => 'users#show'
-
-
+  resources :users, only: [:show, :create]
   resources :genres
+
+  resources :reviews, only: [:create, :updstr:new, :edit, :destroy]
 
 end
