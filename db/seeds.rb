69c391a71ee8ca4976028bc02689285e077b3558
Laura Reynolds <lr.rnlds@gmail.com>
2017-01-10 18:22:46 -0800

    seed update for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@ War Western]
 genres.each do |genre|
   Genre.create(name:genre)
 end
+
+movies = ["Doctor Strange", "Toy Story", "Toy Story 2", "Jurassic Park", "Jurassic World", "The Fall", "Rushmore", "Grand Budapest", "Moana", "The Dark Knight", "The Dark Knight Rises"]
+
+movies.each do |movie|
+  result = OMDB.title(movie)
+  @movie = Movie.create(title: result.title, genre: result.genre, year: result.year, synopsis: result.plot, poster: result.poster )
+end
